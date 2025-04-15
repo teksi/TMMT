75de689508364188caf054e0e8685686c6ba4cc2
Damiano Lombardi <damiano@opengis.ch>
2025-04-15 12:45:05 +0200

    requirements.txt

---
+++ scripts/package-pip-packages.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+pip download -r requirements.txt --only-binary :all: -d temp/
+            unzip -o "temp/*.whl" -d plugin/teksi_module_management_tool/libs
+            rm -r temp
+            # set write rights to group (because qgis-plugin-ci needs it)
+            chmod -R g+w plugin/teksi_module_management_tool/libs
