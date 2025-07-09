770c0adc8898ad64beac3bc576701841cfe0fcc9
Damiano Lombardi <lombardi.damiano@gmail.com>
2025-07-09 11:28:24 +0200

    Package oqtopus from pypi (#20)
    * Package oqtopus from pypi
    * Set oqtopus version to 0.1.16

--- scripts/package-pip-packages.sh
+++ scripts/package-pip-packages.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
-# Remove when oqtopus is available on PyPI
-pip install --target plugin/teksi_module_management_tool/libs git+https://github.com/opengisch/oqtopus.git
-exit 0
-
-pip download -r requirements.txt --only-binary :all: -d temp/
-            unzip -o "temp/*.whl" -d plugin/teksi_module_management_tool/libs
-            rm -r temp
-            # set write rights to group (because qgis-plugin-ci needs it)
-            chmod -R g+w plugin/teksi_module_management_tool/libs
+for LIB in oqtopus pum; do
+  DNL=$(git grep $LIB requirements.txt | cut -d: -f2)
+  pip download $DNL --no-deps --only-binary :all: -d temp/
+done
+unzip -o "temp/*.whl" -d plugin/teksi_module_management_tool/libs
+rm -r temp
+# set write rights to group (because qgis-plugin-ci needs it)
+chmod -R g+w plugin/teksi_module_management_tool/libs
