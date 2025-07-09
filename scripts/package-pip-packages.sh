2de1f6fb0d2308ee82323730b566936ac76ba282
Damiano Lombardi <damiano@opengis.ch>
2025-07-09 11:07:27 +0200

    Set oqtopus version to 0.1.16

--- scripts/package-pip-packages.sh
+++ scripts/package-pip-packages.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-pip download -r requirements.txt --no-deps --only-binary :all: -d temp/
+for LIB in oqtopus pum; do
+  DNL=$(git grep $LIB requirements.txt | cut -d: -f2)
+  pip download $DNL --no-deps --only-binary :all: -d temp/
+done
 unzip -o "temp/*.whl" -d plugin/teksi_module_management_tool/libs
 rm -r temp
 # set write rights to group (because qgis-plugin-ci needs it)
